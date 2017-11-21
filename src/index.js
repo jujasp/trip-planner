0acaea3a3c3ee55c5e2e87591d7a3424f1b8f690
jasminabasurita <jasminejacquelin@gmail.com>
2017-11-21 11:53:41 -0500

    WHYYYYY???

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 const mapboxgl = require("mapbox-gl");
+const marker = require("./marker")
 
 mapboxgl.accessToken = "pk.eyJ1IjoianVqYXNwZXJzIiwiYSI6ImNqYTl0aDM1ZTBsbHkzMnFpd3FvN25ib2gifQ.9ydxBPX7fhz7gSFcx7AkqA";
 
@@ -8,3 +9,12 @@ const map = new mapboxgl.Map({
   zoom: 12, // starting zoom
   style: "mapbox://styles/mapbox/streets-v10" // mapbox has lots of different map styles available.
 });
+
+// const markerDomEl = document.createElement("div"); // Create a new, detached DIV
+// markerDomEl.style.width = "32px";
+// markerDomEl.style.height = "39px";
+// markerDomEl.style.backgroundImage = "url(http://i.imgur.com/WbMOfMl.png)";
+
+// new mapboxgl.Marker(markerDomEl).setLngLat([-74.009, 40.705]).addTo(map); // [-87.6354, 41.8885] for Chicago
+marker('activity', [-74.009, 40.705])
+
---
+++ src/marker.js
@@ -0,0 +1,20 @@
+const mapboxgl = require('mapbox-gl')
+const activity = 'url(http://i.imgur.com/WbMOfMl.png)'
+const hotel = 'url(http://i.imgur.com/D9574Cu.png)'
+const restaurant = 'url(http://i.imgur.com/cqR6pUI.png)'
+
+
+const marker = (activityType, coordinates) => {
+  let img
+  if (activityType === 'activity') img = activity
+  if (activityType === 'hotel') img = hotel
+  if (activityType === 'restaurant') img = restaurant
+  const markerDomEl = document.createElement('div'); // Create a new, detached DIV
+  markerDomEl.style.width = '32px';
+  markerDomEl.style.height = '39px';
+  markerDomEl.style.backgroundImage = img;
+
+  new mapboxgl.Marker(markerDomEl).setLngLat(coordinates).addTo(map);
+}
+
+module.exports = marker
